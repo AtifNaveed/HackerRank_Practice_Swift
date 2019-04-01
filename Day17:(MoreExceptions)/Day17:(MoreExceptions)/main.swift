fc24b5117672e3df3f7d29402a9b9a5d8917a9a2
“Atif <atif.gcucs@gmail.com>
2019-04-01 10:15:59 +0400

    Day17
    Day17

--- Day17:(MoreExceptions)/Day17:(MoreExceptions)/main.swift
+++ Day17:(MoreExceptions)/Day17:(MoreExceptions)/main.swift
@@ -32,3 +32,21 @@ class Calculator {
     } // End of function power
 } // End of class Calculator
 
+let myCalculator = Calculator()
+let t = Int(readLine()!)!
+
+for _ in 0..<t {
+    let np = readLine()!.components(separatedBy: " ").map{ Int($0)! }
+    let n = np[0]
+    let p = np[1]
+    
+    do {
+        let ans = try myCalculator.power(n: n, p: p)
+        print(ans)
+    } catch RangeError.NotInRange(let errorMsg) {
+        print(errorMsg)
+    }
+}
+
+//let ans = try myCalculator.power(n: 3, p: 5)
+//print(ans)
