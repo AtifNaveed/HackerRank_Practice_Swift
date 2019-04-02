4d342e427c56d2ed9d4bd54ddaaa53bffd64342a
“Atif <atif.gcucs@gmail.com>
2019-04-02 12:22:47 +0400

    Day18
    Day18

--- Day18(QueuesAndStacks)/Day18(QueuesAndStacks)/main.swift
+++ Day18(QueuesAndStacks)/Day18(QueuesAndStacks)/main.swift
@@ -35,3 +35,28 @@ class Solution {
 
 }
 
+let s = "madam"
+
+let obj = Solution()
+
+for character in s {
+    obj.pushCharacter(ch: character)
+    obj.enqueueCharacter(ch: character)
+}
+
+
+var isPalindrome = true
+
+for _ in 0..<(s.count / 2) {
+    if obj.popCharacter() != obj.dequeueCharacter() {
+        isPalindrome = false
+        
+        break
+    }
+}
+
+if isPalindrome {
+    print("The word, \(s), is a palindrome.")
+} else {
+    print("The word, \(s), is not a palindrome.")
+}
